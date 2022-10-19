21703c3027755df27655d9c3b940bf78c8089b7e
Brian <bmunoz16048@iesjoanramis.org>
2022-10-19 14:04:43 +0200

    Firts commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory /* setCategories */ }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //console.log(inputValue);
+    if (inputValue.trim().length <= 1) return;
+    //setCategories((categories) => [inputValue, ...categories]);
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar Gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,19 @@
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
+export const GifGrid = ({ category }) => {
+  const { images, isLoading } = useFetchGifs(category);
+  console.log({ /* images, */ isLoading });
+
+  //getGifs(category);
+  return (
+    <>
+      <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
